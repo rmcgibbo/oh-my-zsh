d03cf68a0d975750639c761fcfbb788c51aabf24
Robert McGibbon <rmcgibbo@gmail.com>
2013-05-15 22:46:28 -0600

    Update virtualenv.plugin.zsh

--- plugins/virtualenv/virtualenv.plugin.zsh
+++ plugins/virtualenv/virtualenv.plugin.zsh
@@ -2,7 +2,7 @@ function virtualenv_prompt_info(){
   local virtualenv_path="$VIRTUAL_ENV"
   if [[ -n $virtualenv_path ]]; then
     local virtualenv_name=`basename $virtualenv_path`
-    printf "%s[%s] " "%{${fg[yellow]}%}" $virtualenv_name
+    printf "%s[%s] " "%{${fg[white]}%}" $virtualenv_name
   fi
 }
 
